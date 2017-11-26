8a1d6a1f895adae6262e45a573944be5de2658e7
SannyMichelle <s.speckmeier@goolemail.com>
2017-11-26 18:02:40 +0100

    Änderungen

--- javascript/email.js
+++ javascript/email.js
@@ -3,11 +3,13 @@ function Validierung(email)
 var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 if(email.value.match(mailformat))
 {
+alert("Du hast die erfolgreich für unseren Newsletter angemeldet!")
 document.form1.email.focus();
 return true;
 }
 else
 {
+
 alert("Du hast eine ungültige E-Mail Adresse eingetragen!");
 document.form1.email.focus();
 return false;
