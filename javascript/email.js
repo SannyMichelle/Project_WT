711580f8649ba1f4457ab6e7a852e884d2d4499b
SannyMichelle <s.speckmeier@goolemail.com>
2017-11-26 18:47:33 +0100

    Änderungen

--- javascript/email.js
+++ javascript/email.js
@@ -1,17 +1,14 @@
-function Validierung(email)
-{
-var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-if(email.value.match(mailformat))
-{
-alert("Du hast die erfolgreich für unseren Newsletter angemeldet!")
-document.form1.email.focus();
-return true;
-}
-else
-{
-
-alert("Du hast eine ungültige E-Mail Adresse eingetragen!");
-document.form1.email.focus();
-return false;
-}
+function Validierung(email) {
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (email.value.match(mailformat)) {
+        alert("Du hast dich erfolgreich für unseren Newsletter angemeldet!")
+        document.form2.email.focus();
+        return true;
+    }
+    else {
+        /*Farbe des Textfeldes ändern?!*/
+        alert("Du hast eine ungültige E-Mail Adresse eingetragen!");
+        document.form2.email.focus();
+        return false;
+    }
 }
---
+++ javascript/newsletter.js
@@ -0,0 +1,4 @@
+
+function ja () {
+    document.form1.getElementsByClassName("email");
+}
