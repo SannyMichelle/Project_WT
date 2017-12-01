cb683edc2ae9467ae459d657b9bfd166616f80f4
SannyMichelle <s.speckmeier@goolemail.com>
2017-12-01 10:38:22 +0100

    Änderungen

--- javascript/tabelleSortieren.js
+++ javascript/tabelleSortieren.js
@@ -1,51 +1,51 @@
 function tabelleSortieren(n) {
-
-  var tabelle, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
-  tabelle = document.getElementsByClassName("tabellenformat");
+  var tabelle, zeilen, switching, shouldSwitch, dir, switchcount = 0;
+  tabelle = document.getElementById('tabellenformat');
+  /*Zeilen werden vertauscht, wenn true*/
   switching = true;
-  // Set the sorting direction to ascending:
+  /*Aufsteigende Sortierung*/
   dir = "asc";
-  /* Make a loop that will continue until
-  no switching has been done: */
+
+  /* Ausführung solange bist keine Sortierung stattgefunden hat */
   while (switching) {
-    // Start by saying: no switching is done:
+
     switching = false;
-    rows = tabelle.getElementsByTagName("tr");
-    /* Loop through all tabelle rows (except the
-    first, which contains tabelle headers): */
-    for (i = 1; i < (rows.length - 1); i++) {
-      // Start by saying there should be no switching:
+    zeilen = tabelle.getElementsByTagName("TR");
+
+    /*Schleife über alle Zeile (Spaltenüberschrift ausgenommen)*/
+    for (var i = 1; i < (zeilen.length - 1); i++) {
+      // Standardwert = false
       shouldSwitch = false;
-      /* Get the two elements you want to compare,
-      one from current row and one from the next: */
-      x = rows[i].getElementsByTagName("td")[n];
-      y = rows[i + 1].getElementsByTagName("td")[n];
-      /* Check if the two rows should switch place,
-      based on the direction, asc or desc: */
+
+      /* Elemente vergleichen aktuelle Zeile mit nächster Zeile */
+      var x = zeilen[i].getElementsByTagName("TD")[n];
+      var y = zeilen[i + 1].getElementsByTagName("TD")[n];
+
+      /* Prüft, ob die beiden Zeilen vertauscht werden müssen */
       if (dir == "asc") {
         if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-          // If so, mark as a switch and break the loop:
+          // trifft zu: Zeilen sollen aufsteigend getrauscht werden
           shouldSwitch= true;
           break;
         }
       } else if (dir == "desc") {
         if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-          // If so, mark as a switch and break the loop:
+          // trifft zu: Zeilen sollen absteigend getrauscht werden
           shouldSwitch= true;
           break;
         }
       }
     }
     if (shouldSwitch) {
-      /* If a switch has been marked, make the switch
-      and mark that a switch has been done: */
-      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      /* Wenn ein Tausch durchgeführt werden soll, wird
+      dieser durchgeführt und der Tausch auf true gesetzt*/
+      zeilen[i].parentNode.insertBefore(zeilen[i + 1], zeilen[i]);
       switching = true;
-      // Each time a switch is done, increase this count by 1:
+      // Tauschzähler wird benötigt, um auch absteigend sortieren zu können
       switchcount ++;
     } else {
-      /* If no switching has been done AND the direction is "asc",
-      set the direction to "desc" and run the while loop again. */
+      /* Wenn aufsteigend sortiert wurde und der Zähler bei 0 steht wird die obere
+       Schleife wieder durchlaufen */
       if (switchcount == 0 && dir == "asc") {
         dir = "desc";
         switching = true;
